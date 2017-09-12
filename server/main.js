7099a3ca278da3840e7b5a3ba2d38a8edf494e1f
satyamca1 <satyamca1@gmail.com>
2017-09-12 14:17:04 -0400

    Updated for mongo collection

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ import { Meteor } from 'meteor/meteor';
 //StudentCollection = new Mongo.Collection("studentCollection");
 Meteor.startup(() => {
   // code to run on server at startup
-  var driver = new MongoInternals.RemoteCollectionDriver(MONGO_URL);
+  var driver = new MongoInternals.RemoteCollectionDriver("mongodb://admin:admin@ds147799.mlab.com:47799/studentinformation");
 
   StudentCollection = new Mongo.Collection("studentCollection", { _driver: driver});
 });
